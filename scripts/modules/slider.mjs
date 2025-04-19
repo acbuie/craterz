461b5767c5adbc9f126afeb49a0c597d04674ea7
acbuie <aidancbuie@gmail.com>
2025-04-19 17:32:15 -0600

    fix: Ellipticity is >1, not 0<e<1

--- scripts/modules/slider.mjs
+++ scripts/modules/slider.mjs
@@ -32,12 +32,12 @@ noUiSlider.create(eccenSlider, {
 });
 
 noUiSlider.create(ellipSlider, {
-  start: [0, 1],
+  start: [1, 10],
   connect: true,
   tooltips: [true, true],
   range: {
-    min: 0,
-    max: 1,
+    min: 1,
+    max: 10,
   },
 });
 
