1b4a27f7f32aadf38c8647f2e5ae6a260095fa15
acbuie <aidancbuie@gmail.com>
2025-10-10 10:27:51 -0600

    feat: Improve button clarity with hover tooltip
    Swapped `Clear` and `Reset` button labels for improved semantics.

--- src/components/sidebar/export.tsx
+++ src/components/sidebar/export.tsx
@@ -1,8 +1,15 @@
 "use client";
 
-import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { Info } from "lucide-react";
+
+import { Card, CardContent, CardTitle, CardHeader } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { ButtonGroup } from "../ui/button-group";
+import { ButtonGroup } from "@/components/ui/button-group";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 // TODO: Add button functionality
 // Change button colors
@@ -10,16 +17,32 @@ import { ButtonGroup } from "../ui/button-group";
 export function UpdateFilterCard() {
   return (
     <Card>
-      <CardTitle className="flex justify-center">Search Craters</CardTitle>
+      <CardHeader className="flex justify-center">
+        <CardTitle>Search Craters</CardTitle>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Info className="size-4" />
+          </TooltipTrigger>
+          <TooltipContent>
+            <ul>
+              <li>Click `Apply` to apply a filter.</li>
+              <li>
+                Click `Reset` to reset any changes to the filter configuration.
+              </li>
+              <li>Click `Clear` to clear all applied filters.</li>
+            </ul>
+          </TooltipContent>
+        </Tooltip>
+      </CardHeader>
       <CardContent className="flex justify-between">
         <ButtonGroup>
           <Button size={"sm"}>Apply</Button>
           <Button size={"sm"} variant={"secondary"}>
-            Clear
+            Reset
           </Button>
         </ButtonGroup>
         <Button size={"sm"} variant={"destructive"}>
-          Reset
+          Clear
         </Button>
       </CardContent>
     </Card>
