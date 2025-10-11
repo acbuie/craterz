2c7f0aa4d2a29b1e84df1471f7272135ad3f334e
acbuie <aidancbuie@gmail.com>
2025-10-10 22:04:01 -0600

    feat: Fix header font weight, padding

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,9 +2,9 @@ import { SidebarTrigger } from "./ui/sidebar";
 
 export function AppHeader() {
   return (
-    <header className="flex h-10 shrink-0 items-center gap-2 border-b px-4">
+    <header className="flex h-10 shrink-0 items-center gap-2 border-b px-2">
       <SidebarTrigger />
-      <span className="text-xs md:text-lg flex flex-1 justify-center">
+      <span className="text-xs md:text-lg font-medium flex grow justify-center">
         Craterz, the Mars Crater Database Search Tool
       </span>
     </header>
