11b005bce6090cab09d4f4769f98955c7bc70f84
acbuie <aidancbuie@gmail.com>
2025-10-12 17:41:51 -0600

    feat: Tweak dashboard layout

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -2,11 +2,13 @@ import { AppTable } from "@/components/data-table/data";
 
 export function AppDashboard() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-12 grid-rows-12 gap-2 p-2 h-full">
-      <div className="md:col-span-4 row-span-6 bg-accent rounded-md">
-        <div className="flex justify-center items-center h-full">Chart</div>
+    <div className="grid grid-cols-1 md:grid-cols-12 gap-2 p-2">
+      <div className="md:col-span-4 bg-accent rounded-md">
+        <div className="flex justify-center aspect-square items-center h-full">
+          Chart
+        </div>
       </div>
-      <div className="md:col-span-8 row-span-6 bg-accent rounded-md">
+      <div className="md:col-span-8 bg-accent rounded-md">
         <div className="flex justify-center items-center h-full">Map</div>
       </div>
       <div className="md:col-span-12 row-span-6">
